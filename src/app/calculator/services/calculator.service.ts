7af16d944267660afbcf9cd7eda388954198aea2
Felipe Bueno <felipebueno181@gmail.com>
2020-02-18 22:20:59 -0300

    criando service da calculator

--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -5,5 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorService {
 
+  static readonly SUM: string = "+";
+  static readonly SUBSTRACT: string = "-";
+  static readonly MULTIPLY: string = "*";
+  static readonly DIVISION: string = "/";
+
   constructor() { }
+
+  calcular(num1: number, num2: number, operation: string) : number {
+    let result: number;
+
+    switch(operation){
+      case CalculatorService.SUM:
+        result = num1 + num2;
+        break;
+      case CalculatorService.SUBSTRACT:
+        result = num1 - num2;
+        break;
+      case CalculatorService.MULTIPLY:
+        result = num1 * num2;
+        break;
+      case CalculatorService.DIVISION:
+        result = num1 / num2;
+        break;
+      default:
+        result = 0;
+    }
+
+    return result;
+
+  }
 }

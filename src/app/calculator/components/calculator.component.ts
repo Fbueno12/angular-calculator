539773218c246001b27226be874dc1533d0947a6
Felipe Bueno <felipebueno181@gmail.com>
2020-02-18 23:11:48 -0300

    testes do service e ajuste de operaçoes

--- src/app/calculator/components/calculator.component.ts
+++ src/app/calculator/components/calculator.component.ts
@@ -9,10 +9,101 @@ import { CalculatorService } from '../services';
 })
 export class CalculatorComponent implements OnInit {
 
+  private number1: string;
+  private number2: string;
+  private result: number;
+  private operation: string;
+
   constructor(private calculatorService: CalculatorService) { }
 
   ngOnInit(): void {
-    
+    this.clean();
+  }
+
+  clean(){
+    this.number1   = '0';
+    this.number2   = null;
+    this.result    = null;
+    this.operation = null;
+  }
+
+  addNumber(num: string): void {
+    if (this.operation === null) {
+  	  this.number1 = this.concatNumber(this.number1, num);
+  	} else {
+  	  this.number2 = this.concatNumber(this.number2, num);
+  	}
+  }
+
+  concatNumber(numAtual: string, numConcat: string): string {
+  	// caso contenha apenas '0' ou null, reinicia o valor
+    if (numAtual === '0' || numAtual === null) {
+  	  numAtual = '';
+  	}
+
+    // primeiro dígito é '.', concatena '0' antes do ponto
+  	if (numConcat === '.' && numAtual === '') {
+  	  return '0.';
+  	}
+
+    // caso '.' digitado e já contenha um '.', apenas retorna
+  	if (numConcat === '.' && numAtual.indexOf('.') > -1) {
+  	  return numAtual;
+  	}
+
+  	return numAtual + numConcat;
+  }
+
+  definiroperation(operation: string): void {
+    // apenas define a operação caso não exista uma
+  	if (this.operation === null) {
+      this.operation = operation;
+      return;
+  	}
+
+    /* caso operação definida e número 2 selecionado,
+       efetua o cálculo da operação */
+  	if (this.number2 !== null) {
+  		this.result = this.calculatorService.calcular(
+  			parseFloat(this.number1),
+  			parseFloat(this.number2),
+  			this.operation);
+  		this.operation = operation;
+  		this.number1 = this.result.toString();
+  		this.number2 = null;
+  		this.result = null;
+  	}
+  }
+
+  /**
+   * Efetua o cálculo de uma operação.
+   *
+   * @return void
+   */
+  calcular(): void {
+  	if (this.number2 === null) {
+  		return;
+  	}
+
+  	this.result = this.calculatorService.calcular(
+  		parseFloat(this.number1),
+  		parseFloat(this.number2),
+  		this.operation);
+  }
+
+  /**
+   * Retorna o valor a ser exibido na tela da calculadora.
+   *
+   * @return string
+   */
+  get display(): string {
+  	if (this.result !== null) {
+  		return this.result.toString();
+  	}
+  	if (this.number2 !== null) {
+  		return this.number2;
+  	}
+  	return this.number1;
   }
 
 }
--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
 
@@ -13,4 +13,33 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('must garantee if 1 + 4 is equal to 5',
+     inject(
+      [CalculatorService],
+      ( service: CalculatorService ) => {
+        let sum = service.calcular(1, 4, CalculatorService.SUM);
+        expect(sum).toEqual(5);
+      })
+  );
+
+  it('must garantee if 1 - 1 is equal to 0',
+      inject(
+        [CalculatorService],
+        ( service: CalculatorService ) => {
+          let substract = service.calcular(1, 1, CalculatorService.SUBSTRACT);
+          expect(substract).toEqual(0);
+        }
+      )
+  );
+
+  it('must garantee if 10 / 2 is equal to 5',
+      inject(
+        [CalculatorService],
+        ( service: CalculatorService ) => {
+          let division = service.calcular(10, 2, CalculatorService.DIVISION);
+          expect(division).toEqual(5);
+        }
+      )
+  );
 });

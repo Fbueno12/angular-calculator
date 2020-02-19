871d1cf39869dd89a24a291b3c4e3581d46b7e90
Felipe Bueno <felipebueno181@gmail.com>
2020-02-18 22:04:18 -0300

    criando componente e serviço de calculadora

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CalculatorComponent } from './components';
+import { CalculatorService } from './services';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculatorComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalculatorComponent
+  ],
+  providers: [
+    CalculatorService
   ]
 })
 export class CalculatorModule { }
